b72788d109bd7b7fa8962eecc1f58ee052a577bb
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-05-09 22:19:07 +0700

    Update Program.cs

--- BikeLah_Setel/Program.cs
+++ BikeLah_Setel/Program.cs
@@ -16,7 +16,6 @@ namespace BikeLah_Setel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PagePanduan());
         }
     }
 }
